a9ace3d55851095cb98df46e9453c566d5518edb
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-07-04 00:40:19 +0300

    REST Create and Retrieve Lab (CodeGrade)

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -17,11 +17,26 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(**data)
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.to_dict()), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return jsonify({"error": "Plant not found"}), 404
+        return jsonify(plant.to_dict())
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
